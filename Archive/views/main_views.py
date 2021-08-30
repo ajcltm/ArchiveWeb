2b948b39b6a73fc6e4ca0682d0073e1fafa71877
ajcltm <ajcltm@gmail.com>
2021-08-31 00:15:23 +0900

    2021.8.31. 00:15

--- Archive/views/main_views.py
+++ Archive/views/main_views.py
@@ -1,36 +1,26 @@
 import sqlite3
-
+import pandas as pd
 from datetime import datetime
 from flask import Blueprint, render_template, g
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
-posts = [
-    {
-        'author': {
-            'username':'kim'
-        },
-        'title': 'The first post',
-        'content': 'This is the first post contents',
-        'date_posted': datetime.strptime('2021-8-25', '%Y-%m-%d')
-    },
-    {
-        'author': {
-            'username': 'kim'
-        },
-        'title': 'The second post',
-        'content': 'This is the second post contents',
-        'date_posted': datetime.strptime('2021-8-25', '%Y-%m-%d')
-    }
-]
-
 def get_db():
     return sqlite3.connect('./archive.db')
 
-
 def get_posts_from_db():
     posts = []
-    for row in g.db.execute('select a.id, a.title, b.username, a.post_dated, a.content from post as a join user as b on a.author_id == b.id'):
+    cursor = g.db.cursor()
+    cursor.execute('SELECT * FROM post')
+    rows = cursor.fetchall()
+    # rows=g.db.execute('select a.id, a.title, b.username, a.post_dated, a.content from post as a join user as b on a.author_id == b.id')
+    # print(rows)
+    for row in rows:
+        print(f'test id: {row[0]}')
+        print(f'test author: {row[2]}')
+        print(f'test title: {row[1]}')
+        print(f'test date_posted: {row[3]}')
+        print(f'test: {row[4]}')
         article = {
             'id': row[0],
             'author': {
@@ -40,7 +30,7 @@ def get_posts_from_db():
             'content': row[4],
             'date_posted': datetime.strptime(row[3], '%Y-%m-%d')
         }
-        print(article)
+        # print(article)
         posts.append(article)
     return posts
 
@@ -64,12 +54,14 @@ def index():
 @bp.route('/article/<int:article_id>')
 def article(article_id):
     posts = get_posts_from_db()
+    # print(posts)
     post = None
     for row in posts:
         if row['id'] == article_id:
             post = row
+            # return render_template('content.html', post=post)
             break
-    return render_template('article.html', post=post, posts=posts)
+    return render_template('article.html', post=post)
 
 @bp.route('/about')
 def about():
